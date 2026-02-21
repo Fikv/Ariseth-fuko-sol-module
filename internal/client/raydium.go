d3d8a1eed136e7b06109d431569414bc7f49cc1c
fikv <jakubfik21@gmail.com>
2026-02-21 20:59:53 +0100

    feat: changes backup

---
+++ internal/client/raydium.go
@@ -0,0 +1,17 @@
+package client
+
+
+const baseUrl string = "https://api-v3.raydium.io"
+const version string = "v3"
+
+type path string
+const (
+	Main path = "MAIN"
+	Mint path = "MINT"
+	Pools path = "POOLS"
+	Farms path = "FARMS"
+	Ido path = "IDO" 
+)
+
+
+
