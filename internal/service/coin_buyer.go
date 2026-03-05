f6928f7bdf2694dbe5f29710c3bcf234700c079c
fikv <jakubfik21@gmail.com>
2026-03-05 22:00:30 +0100

    feat: added coin buyer service

---
+++ internal/service/coin_buyer.go
@@ -0,0 +1,95 @@
+package service
+
+import (
+	"context"
+	"errors"
+	"math"
+
+	"ariseth-fuko-sol-module/internal/client"
+	"ariseth-fuko-sol-module/internal/domain"
+)
+
+const (
+	wrappedSOLMint   = "So11111111111111111111111111111111111111112"
+	lamportsPerSOL   = 1_000_000_000
+	defaultSlippage  = 100
+	defaultTxVersion = "V0"
+)
+
+type CoinBuyerService struct {
+	raydiumClient *client.RaydiumClient
+}
+
+func NewCoinBuyerService(raydiumClient *client.RaydiumClient) *CoinBuyerService {
+	if raydiumClient == nil {
+		raydiumClient = client.NewRaydiumClient(nil)
+	}
+
+	return &CoinBuyerService{
+		raydiumClient: raydiumClient,
+	}
+}
+
+func (s *CoinBuyerService) BuyByCA(ctx context.Context, req domain.BuyByCARequest) (domain.BuyByCAResponse, error) {
+	if req.ContractAddress == "" {
+		return domain.BuyByCAResponse{}, errors.New("contractAddress is required")
+	}
+	if req.WalletAddress == "" {
+		return domain.BuyByCAResponse{}, errors.New("walletAddress is required")
+	}
+	if req.SOLAmount <= 0 {
+		return domain.BuyByCAResponse{}, errors.New("solAmount must be greater than zero")
+	}
+
+	slippage := req.SlippageBps
+	if slippage <= 0 {
+		slippage = defaultSlippage
+	}
+	txVersion := req.TxVersion
+	if txVersion == "" {
+		txVersion = defaultTxVersion
+	}
+
+	amountLamports := uint64(math.Round(req.SOLAmount * lamportsPerSOL))
+
+	quote, err := s.raydiumClient.ComputeSwapBaseIn(
+		ctx,
+		wrappedSOLMint,
+		req.ContractAddress,
+		amountLamports,
+		slippage,
+		txVersion,
+	)
+	if err != nil {
+		return domain.BuyByCAResponse{}, err
+	}
+
+	swapResponse := quote
+	if data, ok := quote["data"].(map[string]any); ok {
+		swapResponse = data
+	}
+
+	transaction, err := s.raydiumClient.BuildSwapTransactionBaseIn(
+		ctx,
+		req.WalletAddress,
+		txVersion,
+		req.ComputeUnitPriceMicroLamports,
+		swapResponse,
+		true,
+		false,
+		req.InputTokenAccount,
+		req.OutputTokenAccount,
+	)
+	if err != nil {
+		return domain.BuyByCAResponse{}, err
+	}
+
+	return domain.BuyByCAResponse{
+		InputMint:           wrappedSOLMint,
+		OutputMint:          req.ContractAddress,
+		InputAmountLamports: amountLamports,
+		Quote:               quote,
+		Transaction:         transaction,
+	}, nil
+}
+
